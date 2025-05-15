6b119c0211106adc442d10251ddad9d7ee37fbf2
Lektorius <cslektorius@gmail.com>
2025-05-15 20:01:19 +0300

    feat: enhance navigation menu generation and add examples using for-in loops

--- js/objektai.js
+++ js/objektai.js
@@ -43,3 +43,97 @@ console.log(studentas);
 console.log(studentas.asmenineInfo.vardas);
 console.log(studentas.mokymosiIstaiga.studijuPrograma);
 console.log(studentas.mokymosiIstaiga.pavadinimas);
+console.log('***********************************************');
+// -------------------
+// Pavyzdys su FOR IN ciklu
+// -------------------
+let zmogus = {
+  vardas_pavarde: 'Jonas Jonaitis',
+  amžius: 45,
+  profesija: 'furistas',
+  ūgis: 1.8,
+  ar_turi_teises: true,
+};
+
+for (const raktas in zmogus) {
+    let reiksme = zmogus[raktas];
+    // Paversti didziosiom raidemis
+    if (typeof reiksme == 'string' && raktas == 'vardas_pavarde') {
+        reiksme = reiksme.toUpperCase();
+    }
+    if (raktas == 'amžius') {
+        reiksme = new Date().getFullYear() - reiksme;
+    }
+  console.log(raktas, ':', reiksme);
+}
+
+console.log();
+
+let raktas = 'amžius';
+console.log(raktas, zmogus[raktas]);
+console.log('***********************************************');
+// -------------------
+// Pavyzdys 2
+// -------------------
+studentas = {
+  vardas: 'Tomas',
+  pavarde: 'Tomauskas',
+  amžius: 23,
+  ūgis: 1.7,
+  pazymiai: [7, 7, 8, 7, 9, 8, 10, 9],
+  grupe: 'IFM-3/4',
+};
+
+for (const raktas in studentas) {
+  if (raktas == 'vardas' || raktas == 'pazymiai') {
+    console.log(raktas, studentas[raktas]);
+  }
+}
+
+console.log('-------------');
+
+for (const raktas in studentas) {
+  if (typeof studentas[raktas] == 'object') {
+    console.log(raktas, studentas[raktas]);
+  }
+}
+console.log('***********************************************');
+
+// -------------------
+// Pavyzdys 3
+// Navigacijos meniu generavimas
+// -------------------
+let meniu = [
+    {
+        pavadinimas: 'Pagrindinis',
+        nuoroda: 'index.html',
+        ikona: 'home.png',
+    },
+    {
+        pavadinimas: 'Paslaugos',
+        nuoroda: 'paslaugos.html',
+    },
+    {
+        pavadinimas: 'Galerija',
+        nuoroda: 'galerija.html',
+    }
+];
+
+let meniuHTML = '<ul>';
+
+    for (const elementas of meniu) {
+        meniuHTML += '<li>';
+            meniuHTML += '<a href="' + elementas.nuoroda + '">';
+                if (elementas.ikona) {
+                    meniuHTML += '<img src="' + elementas.ikona + '" alt="Ikona">'; 
+                }
+                meniuHTML += elementas.pavadinimas;
+            meniuHTML += '</a>';
+        meniuHTML += '</li>';
+    }
+
+meniuHTML += '</ul>';
+
+console.log(meniuHTML);
+document.getElementById('desktop-meniu').innerHTML = meniuHTML;
+// -------------------
