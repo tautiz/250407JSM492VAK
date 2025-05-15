895f1b6050396e1740d5dacce13de0eda44f4323
Lektorius <cslektorius@gmail.com>
2025-05-15 21:59:51 +0300

    feat: add student object example with average grade calculation and update navigation menu comment

--- js/objektai.js
+++ js/objektai.js
@@ -135,5 +135,27 @@ let meniuHTML = '<ul>';
 meniuHTML += '</ul>';
 
 console.log(meniuHTML);
-document.getElementById('desktop-meniu').innerHTML = meniuHTML;
+//document.getElementById('desktop-meniu').innerHTML = meniuHTML;
 // -------------------
+
+// Pavyzdys 4
+studentas = {
+  vardas_pavarde: 'Tomas Tomauskas',
+  pazymiai: [7, 7, 8, 7, 9, 8, 8],
+  vidurkis: function() {
+    let suma = 0;
+    for (const paz of this.pazymiai) {
+      suma += paz;
+    }
+
+    return suma / this.pazymiai.length;
+  }
+};
+
+console.log('STUDENTAS', studentas, '\n');
+console.log('JO PAZYMIAI', studentas.pazymiai, '\n');
+console.log('PAZYMIU VIDURKIS', studentas.vidurkis());
+console.log('***********************************************');
+// -------------------
+
+// Pavyzdys 5
